8c689511c6c7caa47280338d4c51637a487d7929
olutunde22 <solabi.tunde@gmail.com>
2024-04-23 08:08:37 +0100

    chore: add more project

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -11,7 +11,15 @@ export async function GET(request: Request) {
       created_at: 'Apr 2024'
     },
     {
-      id: '2',
+      id:'2',
+      url: 'https://minkajobs.com/',
+      imageUrl:'https://res.cloudinary.com/dixvcadud/image/upload/v1713855449/minka.png',
+      name: 'Minka Health',
+      description: 'Minka Health is a progressive startup operating at the intersection of healthcare, education, and staffing. We are dedicated to addressing the pressing issue of labor shortages in the healthcare sector. Our platform serves as a conduit, connecting healthcare professionals with the institutions that need them most',
+      created_at: 'November 2023'
+    },
+    {
+      id: '3',
       url: 'https://app.spursmb.com/',
       imageUrl: 'https://res.cloudinary.com/dixvcadud/image/upload/v1713854769/spur.png',
       name: 'Spur SMB',
@@ -19,13 +27,13 @@ export async function GET(request: Request) {
       created_at: 'September 2023'
     },
     {
-      id:'3',
-      url: 'https://minkajobs.com/',
-      imageUrl:'https://res.cloudinary.com/dixvcadud/image/upload/v1713855449/minka.png',
-      name: 'Minka Health',
-      description: 'Minka Health is a progressive startup operating at the intersection of healthcare, education, and staffing. We are dedicated to addressing the pressing issue of labor shortages in the healthcare sector. Our platform serves as a conduit, connecting healthcare professionals with the institutions that need them most',
-      created_at: 'November 2023'
-    },
+      id: '4',
+      url: 'https://www.teldev.org/',
+      imageUrl: 'https://res.cloudinary.com/dixvcadud/image/upload/v1713856063/teldev.png',
+      name: 'TelDev',
+      description: 'TelDev is a technology-based organization with the goal of creating a system where technology and solutions to tech-related difficulties are no longer out of reach and can be supplied to you with ease.',
+      created_at: 'February 2023'
+    }
   ]
   return NextResponse.json(projects);
 }
